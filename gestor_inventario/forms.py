bafbb507c0e4d381e1b21cd6167cd3625d363ec8
NicolásHuenchuquen <nicolas.huenchuquen.r@gmail.com>
2024-11-11 18:25:58 -0300

    Se crea: formulario de ingreso de activos y validaciones, crud con datatablesy sus funciones de crear y borrar, queda pendiente funcion de editar, ademas se permite la descarga de los elementos de datables a excel, pdf y para imprimir.(todo lo completado con sus correspondientes vistas)

--- gestor_inventario/forms.py
+++ gestor_inventario/forms.py
@@ -3,16 +3,16 @@ from .models import Articulo
 from django.core.exceptions import ValidationError
 from django.core import validators
 
-def validate_cod_articulo(value):
+def validar_cod_articulo(value):
     if not (value.startswith('CFTN') or value.startswith('IPN')):
         raise ValidationError(
-            'El código de artículo debe comenzar con "CFTN" o "IPN".'
+            'El código del artículo debe comenzar con "CFTN" o "IPN".'
         )
 
-class FormArticulo(forms.ModelForm):
+class FormArticuloInsumo(forms.ModelForm):
     
     cod_articulo = forms.CharField(
-        validators=[validators.MinLengthValidator(4), validators.MaxLengthValidator(8),validate_cod_articulo],
+        validators=[validators.MinLengthValidator(4), validators.MaxLengthValidator(8),validar_cod_articulo],
         widget=forms.TextInput(attrs={'class': 'form-control', 'style':'margin-top: 13px'}),
     )
 
@@ -22,7 +22,7 @@ class FormArticulo(forms.ModelForm):
     )
 
     cantidad = forms.IntegerField(
-        min_value=1,  # Asegúrate de que la cantidad sea al menos 1
+        min_value=1, 
         widget=forms.NumberInput(attrs={'class': 'form-control'}),
         error_messages={
             'min_value': 'La cantidad debe ser un número positivo.'
@@ -34,10 +34,65 @@ class FormArticulo(forms.ModelForm):
         widget=forms.Select(attrs={'class': 'form-select'})
     )
 
-    tipo_articulo = forms.ChoiceField(
-        choices=[('Insumo', 'INSUMO'), ('Activo', 'ACTIVO')],
-        widget=forms.Select(attrs={'class': 'form-select', 'style':'margin-top: 13px'})
+    tipo_articulo = forms.CharField(
+    initial='Insumo',  # Valor predeterminado
+    widget=forms.TextInput(attrs={
+        'class': 'form-control',
+        'style':'margin-top: 13px',
+        'readonly': 'readonly', 
+    })
     )
+
+
+    ubicacion = forms.CharField(
+        validators=[validators.MinLengthValidator(2), validators.MaxLengthValidator(8)],
+        widget=forms.TextInput(attrs={'class': 'form-control'})
+    )
+
+    observacion = forms.CharField(
+        required=False,
+        validators=[validators.MinLengthValidator(0)],
+        widget=forms.Textarea(attrs={'class': 'form-control', 'rows': 3})
+    )
+    
+    class Meta:
+        model = Articulo
+        fields = '__all__'
+
+class FormArticuloActivo(forms.ModelForm):
+    
+    cod_articulo = forms.CharField(
+        validators=[validators.MinLengthValidator(4), validators.MaxLengthValidator(8),validar_cod_articulo],
+        widget=forms.TextInput(attrs={'class': 'form-control', 'style':'margin-top: 13px'}),
+    )
+
+    nombre = forms.CharField(
+        validators=[validators.MinLengthValidator(3), validators.MaxLengthValidator(100)],
+        widget=forms.TextInput(attrs={'class': 'form-control'})
+    )
+
+    cantidad = forms.IntegerField(
+        min_value=1, 
+        widget=forms.NumberInput(attrs={'class': 'form-control'}),
+        error_messages={
+            'min_value': 'La cantidad debe ser un número positivo.'
+        }
+    )
+
+    grupo = forms.ChoiceField(
+        choices=[('CFTN', 'CFTN'), ('IPN', 'IPN')],
+        widget=forms.Select(attrs={'class': 'form-select'})
+    )
+
+    tipo_articulo = forms.CharField(
+    initial='Activo',  
+    widget=forms.TextInput(attrs={
+        'class': 'form-control',
+        'style':'margin-top: 13px',
+        'readonly': 'readonly', 
+    })
+    )
+
 
     ubicacion = forms.CharField(
         validators=[validators.MinLengthValidator(2), validators.MaxLengthValidator(8)],
