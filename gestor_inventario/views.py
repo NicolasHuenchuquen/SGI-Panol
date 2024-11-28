97e1c085223154c65c0ed75e1d86f290580a3474
NicolasHuenchuquen <nicolas.huenchuquen.r@gmail.com>
2024-11-28 02:24:33 -0300

    Comentarios de codigo, implementacion nuevamente del boton eliminar para manejar artiuclos erroneos

--- gestor_inventario/views.py
+++ gestor_inventario/views.py
@@ -9,26 +9,42 @@ from django.contrib import messages
 
 @login_required(login_url='sesion_cerrada')
 def tabla_articulos(request):
+    """
+    Vista para mostrar la tabla con todos los artículos que no están dados de baja.
+    Se recuperan todos los artículos de la base de datos que no tienen el campo 'dado_de_baja' en True,
+    y se pasan al contexto para mostrarlos en la vista.
+    """
     articulos = Articulo.objects.filter(dado_de_baja=False)
     data = {'articulos' : articulos}
     return render(request, 'inventario/tabla_articulos.html', data)
 
 @login_required(login_url='sesion_cerrada')
 def tabla_insumos(request):
-    articulos = Articulo.objects.filter(tipo_articulo="Insumo",dado_de_baja=False)
+    """
+    Vista para mostrar la tabla con todos los artículos del tipo "Insumo" que no están dados de baja.
+    Filtra los artículos por tipo y por el estado de baja, y pasa los datos a la vista correspondiente.
+    """
+    articulos = Articulo.objects.filter(tipo_articulo="Insumo", dado_de_baja=False)
     data = {'articulos': articulos}
     return render(request, 'inventario/tabla_insumos.html', data)
 
 @login_required(login_url='sesion_cerrada')
 def tabla_activos(request):
-    articulos = Articulo.objects.filter(tipo_articulo="Activo",dado_de_baja=False)
+    """
+    Vista para mostrar la tabla con todos los artículos del tipo "Activo" que no están dados de baja.
+    Filtra los artículos por tipo "Activo" y por el estado de baja, y pasa los datos a la vista correspondiente.
+    """
+    articulos = Articulo.objects.filter(tipo_articulo="Activo", dado_de_baja=False)
     data = {'articulos': articulos}
     return render(request, 'inventario/tabla_activos.html', data)
 
-
-
 @login_required(login_url='sesion_cerrada')
 def agregar_insumo(request):
+    """
+    Vista para agregar un nuevo insumo al inventario.
+    Si se recibe una solicitud POST con datos válidos, se guarda el insumo y se redirige a la misma vista para mostrar un mensaje de éxito.
+    Si no, se muestra el formulario vacío.
+    """
     form = FormArticuloInsumo()
     if request.method == 'POST':
         form = FormArticuloInsumo(request.POST)
@@ -41,23 +57,31 @@ def agregar_insumo(request):
 
 @login_required(login_url='sesion_cerrada')
 def agregar_activo(request):
+    """
+    Vista para agregar un nuevo activo al inventario.
+    Similar a la vista de agregar insumo, pero para agregar activos.
+    Si se recibe una solicitud POST con datos válidos, se guarda el activo y se redirige a la misma vista para mostrar un mensaje de éxito.
+    Si no, se muestra el formulario vacío y, en caso de error, se imprimen los errores del formulario.
+    """
     form = FormArticuloActivo()
     if request.method == 'POST':
         form = FormArticuloActivo(request.POST)
         if form.is_valid():
-            
             form.save()
             messages.success(request, 'El Activo ha sido ingresado correctamente.', extra_tags='agregar_activo')
             return redirect('agregar_activo')
         else:
-            print("Formulario no válido", form.errors)  # Mostrara los errores del formulario
+            print("Formulario no válido", form.errors)  # Mostrará los errores del formulario
     return render(request, 'inventario/agregar_activo.html', {'form': form})
-
 
 @login_required(login_url='sesion_cerrada')
 def editar_articulo(request):
+    """
+    Vista para editar un artículo existente en el inventario.
+    Si se recibe una solicitud POST con los datos del formulario, se actualiza el artículo y se redirige a la vista correspondiente según el origen de la tabla.
+    Si solo se recibe el código del artículo, se muestra el formulario para editar el artículo.
+    """
     if request.method == 'POST':
-        # Obtiene el código del artículo del form
         cod_articulo = request.POST.get('cod_articulo')
         tabla_origen = request.POST.get('tabla_origen')
         
@@ -67,36 +91,28 @@ def editar_articulo(request):
         
         articulo = get_object_or_404(Articulo, cod_articulo=cod_articulo)
         
-        # Si viene el formulario completo (con los campos del artículo)
-        if 'nombre' in request.POST:  # o cualquier campo de tu formulario
+        if 'nombre' in request.POST:  # Si se recibe el formulario completo con los campos del artículo
             form = FormArticuloEditar(request.POST, instance=articulo)
             if form.is_valid():
                 form.save()
                 messages.success(request, 'Artículo guardado correctamente.', extra_tags='articulo_editado')
                 
-                # Redirige según origen
+                # Redirige según el origen de la tabla
                 if tabla_origen == 'tabla_insumos':
                     return redirect('tabla_insumos')
-                
                 elif tabla_origen == 'tabla_activos':
                     return redirect('tabla_activos')
-                
                 elif tabla_origen == 'tabla_general':
                     return redirect('tabla_articulos')
-                
                 elif tabla_origen == 'General':
                     return redirect('tabla_bajas')
-                
                 elif tabla_origen == 'Insumo_baja':
                     return redirect('tabla_bajas_tipo', tipo='Insumo_baja')
-                
                 elif tabla_origen == 'Activo_baja':
                     return redirect('tabla_bajas_tipo', tipo='Activo_baja')
-                
                 return redirect('tabla_bajas')
         else:
-            # Si solo viene el código, muestra el formulario
-            form = FormArticuloEditar(instance=articulo)
+            form = FormArticuloEditar(instance=articulo)  # Si solo se recibe el código, muestra el formulario
         
         data = {
             'form': form,
@@ -106,18 +122,21 @@ def editar_articulo(request):
         
         return render(request, 'inventario/editar_articulo.html', data)
     
-    # Si alguien intenta acceder por GET, redirigir
-    return redirect('tabla_articulos')
-
+    return redirect('tabla_articulos')  # Si alguien intenta acceder por GET, redirigir
 
 @login_required(login_url='sesion_cerrada')
 def dar_de_baja_articulo(request):
+    """
+    Vista para dar de baja un artículo existente.
+    Cambia el estado del artículo a dado de baja y redirige según el origen de la tabla.
+    """
     cod_articulo_form = request.POST.get('cod_articulo')
     tabla_origen = request.POST.get('tabla_origen')
     
     articulo = get_object_or_404(Articulo, cod_articulo=cod_articulo_form)
     articulo.dado_de_baja = True
     articulo.save()
+    
     # Redirige según el origen
     if tabla_origen == 'tabla_insumos':
         return redirect('tabla_insumos')
@@ -125,11 +144,13 @@ def dar_de_baja_articulo(request):
         return redirect('tabla_activos')
     else:
         return redirect('tabla_articulos')
-    
-
 
 @login_required(login_url='sesion_cerrada')
 def tabla_bajas(request, tipo=None):
+    """
+    Vista para mostrar la tabla de artículos dados de baja.
+    Dependiendo del tipo (Insumo, Activo, General), muestra los artículos correspondientes.
+    """
     if tipo == "Insumo_baja":
         articulos = Articulo.objects.filter(dado_de_baja=True, tipo_articulo="Insumo")
     elif tipo == "Activo_baja":
@@ -143,19 +164,20 @@ def tabla_bajas(request, tipo=None):
     }
     return render(request, 'inventario/tabla_bajas.html', data)
 
-
 @login_required(login_url='sesion_cerrada')
 def cancelar_baja(request):
+    """
+    Vista para cancelar la baja de un artículo.
+    Cambia el estado del artículo a no dado de baja y redirige según el origen de la tabla.
+    """
     cod_articulo_form = request.POST.get('cod_articulo')
     tipo_tabla = request.POST.get('tipo_tabla')  # Puede ser "Insumo_baja", "Activo_baja" o "General" (valor predeterminado)
 
-    # Recuperar y actualizar el estado del artículo
     articulo = get_object_or_404(Articulo, cod_articulo=cod_articulo_form)
     articulo.dado_de_baja = False
     articulo.save()
     
-
-    # Redirigir según el origen de la tabla
+    # Redirige según el origen de la tabla
     if tipo_tabla == 'Insumo_baja':
         return redirect('tabla_bajas_tipo', tipo='Insumo_baja')
     elif tipo_tabla == 'Activo_baja':
@@ -163,19 +185,21 @@ def cancelar_baja(request):
     else:  # General
         return redirect('tabla_bajas')
 
+@login_required(login_url='sesion_cerrada')
+def eliminar_articulo(request):
+    """
+    Vista para eliminar un artículo del inventario.
+    Elimina el artículo y redirige según el origen de la tabla (Insumo, Activo, General).
+    """
+    cod_articulo_form = request.POST.get('cod_articulo')
+    tipo_tabla = request.POST.get('tipo_tabla')  # Puede ser "Insumo", "Activo" o "General" (valor predeterminado)
+    articulo = Articulo.objects.get(cod_articulo=cod_articulo_form)
+    articulo.delete()
 
-#CODIGO PARA ELIMINAR ARTICULOS DADOS DE BAJA SI EN ALGUN MOMENTO SE NECESITA
-# @login_required(login_url='sesion_cerrada')
-# def eliminar_articulo(request):
-#     cod_articulo_form = request.POST.get('cod_articulo')
-#     tipo_tabla = request.POST.get('tipo_tabla')  # Puede ser "Insumo", "Activo" o "General" (valor predeterminado)
-#     articulo = Articulo.objects.get(cod_articulo = cod_articulo_form)
-#     articulo.delete()
-
-#     # Redirigir según el origen de la tabla
-#     if tipo_tabla == 'Insumo':
-#         return redirect('tabla_bajas_tipo', tipo='Insumo')
-#     elif tipo_tabla == 'Activo':
-#         return redirect('tabla_bajas_tipo', tipo='Activo')
-#     else:  # General
-#         return redirect('tabla_bajas')
+    # Redirige según el origen de la tabla
+    if tipo_tabla == 'Insumo':
+        return redirect('tabla_insumos')
+    elif tipo_tabla == 'Activo':
+        return redirect('tabla_activos')
+    else:
+        return redirect('tabla_articulos')
--- sgipañol/views.py
+++ sgipañol/views.py
@@ -5,43 +5,109 @@ from django.contrib import messages
 #PARA MANEJAR EL ERROR DE CSRF TOKEN
 from django.shortcuts import render
 
+
+
 @login_required(login_url='sesion_cerrada')
 def navbar(request):
+    """
+    Vista que renderiza la barra de navegación solo para usuarios autenticados.
+    
+    Esta vista requiere que el usuario esté autenticado. Si no lo está, lo redirige a la página de 
+    inicio de sesión, indicada en `login_url='sesion_cerrada'`.
+
+    Parameters:
+    - request: El objeto de solicitud HTTP.
+
+    Returns:
+    - render: Renderiza la plantilla 'navegacion/nav.html' que muestra la barra de navegación.
+    """
     return render(request, 'navegacion/nav.html')
 
 def iniciar_sesion(request):
+    """
+    Vista para iniciar sesión en el sistema.
+
+    Permite a los usuarios autenticarse mediante su nombre de usuario y contraseña. Si los datos son correctos,
+    el usuario será autenticado y redirigido a la página que intentaba acceder o, si no se especificó una página,
+    al formulario de solicitudes.
+
+    Si las credenciales no son válidas, se muestra un mensaje de error al usuario.
+
+    Parameters:
+    - request: El objeto de solicitud HTTP.
+
+    Returns:
+    - render: Renderiza la plantilla 'navegacion/iniciar_sesion.html' para mostrar el formulario de inicio de sesión.
+    - redirect: Si la autenticación es exitosa, redirige al usuario a la URL indicada.
+    """
     if request.method == 'POST':
+        # Obtiene el nombre de usuario y la contraseña desde el formulario de inicio de sesión
         username = request.POST.get('username')
         password = request.POST.get('password')
-        
+
+        # Verifica si ambos campos han sido llenados
         if not username or not password:
+            # Si faltan datos, muestra un mensaje de error
             messages.error(request, 'Por favor ingrese tanto el nombre de usuario como la contraseña.', extra_tags='login_error')
         else:
-            # Autenticación del usuario
+            # Autentica al usuario con las credenciales proporcionadas
             user = authenticate(request, username=username, password=password)
             if user is not None:
+                # Si la autenticación es exitosa, inicia sesión y redirige
                 login(request, user)
-                next_url = request.GET.get('next', 'navbar')  # Redirigir a la URL siguiente o 'navbar'
+                # Obtiene la URL a la que se debe redirigir (si existe en la petición GET, sino redirige a 'formulario_solicitudes')
+                next_url = request.GET.get('next', 'formulario_solicitudes')
                 return redirect(next_url)
             else:
+                # Si las credenciales no son válidas, muestra un mensaje de error
                 messages.error(request, 'Nombre de usuario o contraseña no válidos', extra_tags='login_error')
-    
-    return render(request, 'navegacion/iniciar_sesion.html')
 
+    # Renderiza el formulario de inicio de sesión
+    return render(request, 'navegacion/iniciar_sesion.html')
 
 @login_required(login_url='sesion_cerrada')
 def cerrar_sesion(request):
+    """
+    Vista para cerrar sesión del usuario autenticado.
+
+    Cuando el usuario cierra sesión, la sesión se destruye y se redirige al formulario de inicio de sesión.
+
+    Parameters:
+    - request: El objeto de solicitud HTTP.
+
+    Returns:
+    - redirect: Redirige al usuario a la página de inicio de sesión ('iniciar_sesion').
+    """
+    # Cierra la sesión del usuario
     logout(request)
+    # Redirige al formulario de inicio de sesión
     return redirect('iniciar_sesion')
 
 
+
 def csrf_error(request, reason=""):
-    # Detecta la URL de origen del error
-    referer = request.META.get('HTTP_REFERER', '/')
+    """
+    Maneja los errores de CSRF (Cross-Site Request Forgery) y presenta una página de error adecuada.
+
+    Cuando un error CSRF ocurre, esta función es llamada para informar al usuario sobre el error y permitir
+    que puedan regresar a la página de origen desde donde intentaron realizar una acción no válida debido a CSRF.
     
-    # Renderiza la misma página con un indicador de error
+    Parameters:
+    - request: El objeto de solicitud HTTP.
+    - reason: El motivo del error (por defecto está vacío, pero puede ser útil para dar más detalles sobre la causa).
+
+    Returns:
+    - render: Retorna una respuesta HTTP con el renderizado de la página de error CSRF.
+    """
+
+    # Obtiene la URL desde la que el usuario intentaba hacer la solicitud (HTTP_REFERER) para poder redirigirlo
+    # de vuelta a esa página después del error, o al inicio si no se encuentra dicha información.
+    referer = request.META.get('HTTP_REFERER', '/')
+
+    # Renderiza la página de error CSRF, pasando un indicador de error y la razón del error si es proporcionada.
     return render(request, 'navegacion/csrf_error.html', {
-        'error_csrf': True,
-        'reason': reason,
-        'referer': referer,
+        'error_csrf': True,   # Indicador de que hubo un error CSRF
+        'reason': reason,      # Motivo del error (opcional, dependiendo de la causa específica)
+        'referer': referer,    # La URL de la que el usuario provino, para redirigirlo o devolverlo a esa página.
     })
+
